eff69c5d246005f437a628532f6f300d983712d4
ashishbw <techackernp@gmail.com>
2023-11-30 09:53:08 +0545

    django added

---
+++ studentcms/accounts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Profile(models.Model):
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	email = models.CharField(
+			max_length=50,
+			null=False,
+			unique=True
+			)
+	password = models.CharField(max_length=40)
+	phone = models.IntegerField()
+
+
