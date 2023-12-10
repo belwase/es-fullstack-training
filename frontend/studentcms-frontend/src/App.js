52714ab60fed72a1d72cd7b6aede413b4e965d75
ashishbw <techackernp@gmail.com>
2023-12-10 07:51:28 +0545

    homepage with headers,footers

--- frontend/studentcms-frontend/src/App.js
+++ frontend/studentcms-frontend/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import HomePage from './HomePage';
+// import StudentsPage from './Students';
+// import CoursesPage from './Courses';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/students" element={<StudentsPage />} />
+            <Route path="/courses" element={<CoursesPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/studentcms-frontend/src/Header.js
@@ -0,0 +1,19 @@
+// Header.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/students">Students</Link></li>
+          <li><Link to="/courses">Courses</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
