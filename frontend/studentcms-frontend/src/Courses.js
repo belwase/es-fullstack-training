ad95477ed31c515b68ffac147b772351aeac0382
ashishbw <techackernp@gmail.com>
2023-12-12 08:55:14 +0545

    student course assignment

--- frontend/studentcms-frontend/src/Courses.js
+++ frontend/studentcms-frontend/src/Courses.js
@@ -1,13 +1,98 @@
 // CoursesPage.js
-import React, { useRef } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import API_URL from './config';
 
 const CoursesPage = () => {
   // Generate random data for demonstration
-  const courses = [
-    { id: 101, title: 'React Basics', instructor: 'John Smith' },
-    { id: 102, title: 'JavaScript Fundamentals', instructor: 'Jane Doe' },
-    { id: 103, title: 'Web Development with HTML/CSS', instructor: 'Bob Johnson' },
-  ];
+  const [courses, setCourses] = useState([]);
+  const [students, setStudents] = useState([]);
+  const [selectedCourse, setSelectedCourse] = useState('');
+  const [selectedStudent, setSelectedStudent] = useState('');
+
+  useEffect(() => {
+    
+      const fetchCourses = async () => {
+        try {
+          const response = await axios.get(`${API_URL}/course/`);
+          setCourses(response.data.data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+      const fetchStudents = async () => {
+        try {
+          const response = await axios.get(`${API_URL}/student/`);
+          setStudents(response.data.data);
+          console.log(students)
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+    fetchCourses();
+    fetchStudents();
+  }, []);
+
+  const onStudentChange = (e) => {
+    setSelectedStudent(e.target.value)
+  }
+
+  const onCourseChange = (e) => {
+    setSelectedCourse(e.target.value)
+  }
+
+  const AddAssignment = (e) => {
+    e.preventDefault();
+    
+    const Add = async () => {
+      try{
+
+        var headers = {
+          'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6InJhbSJ9.ZlRJryXSibFMHwtWEPLCqXNjS3NGD415Gub0lQPt474',
+          'Content-Type': 'application/json'
+        }
+
+        var payload = {
+          "student_id": selectedStudent,
+          "course_id": selectedCourse
+        };
+        var response = await axios.post(
+            `${API_URL}/student-assignment/`,
+            payload,
+            {headers: headers}
+            )
+
+        if(response.data.data == null){
+          var error = "";
+          let i = 0;
+          for(i=0;i<response.data.messages.length;i++){
+            error += response.data.messages[i] + '. ';
+          }
+          toast.error(error, {
+            position: toast.POSITION.TOP_RIGHT
+          });
+        }else{
+          toast.success("Student-Assignment Added !", {
+            position: toast.POSITION.TOP_RIGHT
+          });
+        }
+        
+        console.log(response)
+        
+
+      }catch(err){
+        console.log("Got error ", err)
+      }
+
+    }
+
+    Add()
+    
+
+  }
+  
 
   return (
     <div style={pageStyle}>
@@ -16,20 +101,46 @@ const CoursesPage = () => {
         <thead>
           <tr>
             <th>ID</th>
-            <th>Title</th>
-            <th>Instructor</th>
+            <th>Name</th>
+            
           </tr>
         </thead>
         <tbody>
           {courses.map(course => (
             <tr key={course.id}>
               <td>{course.id}</td>
-              <td>{course.title}</td>
-              <td>{course.instructor}</td>
+              <td>{course.name}</td>
+              
             </tr>
           ))}
         </tbody>
       </table>
+
+      <div class="col-md-6">
+            <h2>Assignment Form</h2>
+            <form onSubmit="AddAssignment">
+                <div class="mb-3">
+                    <label for="studentDropdown" class="form-label">Select Student</label>
+                    <select class="form-select" id="student_id" name="student" onChange="onStudentChange">
+                       {students.map(student => (
+                          <option value={ student.id }>{student.first_name}</option>
+                        ))}
+                            
+                     
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="courseDropdown" class="form-label">Select Course</label>
+                    <select class="form-select" id="course_id" name="course"  onChange="onCourseChange">
+                        {courses.map(course => (
+                          <option value={ course.id }>{course.name}</option>
+                        ))}
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Assign Course</button>
+            </form>
+        </div>
+
     </div>
   );
 }
--- frontend/studentcms-frontend/src/Students.js
+++ frontend/studentcms-frontend/src/Students.js
@@ -1,6 +1,8 @@
 // StudentsPage.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import API_URL from './config';
 
 
@@ -14,12 +16,15 @@ const StudentsPage = () => {
               password: '',
               phone: ''
             })
+
+  //const notify = () => { toast("Student Added !" ) };
   
   useEffect(() => {
     console.log("initialized.")
       const fetchData = async () => {
         try {
           const response = await axios.get(`${API_URL}/student/`);
+
           console.log(response.data.data)
           setStudents(response.data.data);
           
@@ -34,67 +39,156 @@ const StudentsPage = () => {
   }, []);
 
   const handleAddStudent = (e) => {
+    e.preventDefault();
+    
+    const AddStudent = async () => {
+      try{
+
+        var headers = {
+          'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6InJhbSJ9.ZlRJryXSibFMHwtWEPLCqXNjS3NGD415Gub0lQPt474',
+          'Content-Type': 'application/json'
+        }
+
+        var payload = newStudent;
+        var response = await axios.post(
+            `${API_URL}/student/`,
+            payload,
+            {headers: headers}
+            )
+
+        if(response.data.data == null){
+          var error = "";
+          let i = 0;
+          for(i=0;i<response.data.messages.length;i++){
+            error += response.data.messages[i] + '. ';
+          }
+          toast.error(error, {
+            position: toast.POSITION.TOP_RIGHT
+          });
+        }else{
+          toast.success("Student Added !", {
+            position: toast.POSITION.TOP_RIGHT
+          });
+        }
+        
+        console.log(response)
+        
+
+      }catch(err){
+        console.log("Got error ", err)
+      }
+
+    }
+
+    AddStudent()
+    
 
   }
+
 
   const handleInputChange = (e) => {
     var field_name = e.target.name;
     var field_value = e.target.value;
     console.log(field_name, field_value);
-    
+    setNewStudent((prevStudent) => ({ ...prevStudent, [field_name]: field_value }));
+    console.log(newStudent)
 
   }
 
   return (
-    <div style={pageStyle}>
-      <h2>Students Page</h2>
-      <table style={tableStyle}>
+    <>
+    <ToastContainer />
+    <div style={styles.page}>
+      <h2 style={styles.heading}>Students Page</h2>
+      <table style={styles.table}>
         <thead>
           <tr>
             <th>ID</th>
             <th>Name</th>
-            <th>Age</th>
           </tr>
         </thead>
         <tbody>
           {students.map(student => (
-            <tr key={student.id} style={{ 
-              backgroundColor: student.age > 20 ? 'green' : 'inherit' }}>
+            <tr key={student.id}
+                style={{
+                ...styles.tableRow,
+                backgroundColor: student.age > 20 ? 'green' : 'inherit',
+                color: student.age > 20 ? 'white' : 'inherit',
+              }}>
               <td>{student.id}</td>
               <td>{student.first_name}</td>
-              <td>{student.age}</td>
             </tr>
           ))}
         </tbody>
       </table>
-      <form onSubmit={handleAddStudent}>
-        <label>
+      <form onSubmit={handleAddStudent} style={{ marginTop: '20px' }}>
+        <label  style={styles.formLabel}>
           First Name:
           <input type="text" name="first_name" value={newStudent.first_name} onChange={handleInputChange} required />
         </label>
-        <label>
+        <label  style={styles.formLabel}>
           Last Name:
           <input type="text" name="last_name" value={newStudent.last_name} onChange={handleInputChange} required />
         </label>
-        <label>
+        <label  style={styles.formLabel}>
           Email:
           <input type="text" name="email" value={newStudent.email} onChange={handleInputChange} required />
         </label>
-        <label>
+        <label  style={styles.formLabel}>
           Password:
           <input type="text" name="password" value={newStudent.password} onChange={handleInputChange} required />
         </label>
-        <label>
+        <label  style={styles.formLabel}>
           Phone:
           <input type="number" name="phone" value={newStudent.phone} onChange={handleInputChange} required />
         </label>
-        <button type="submit">Add Student</button>
+        <button type="submit" style={styles.addButton}>
+          Add Student
+        </button>
       </form>
     </div>
+
+    </>
   );
 }
 
-// Styles
+const styles = {
+  page: {
+    fontFamily: 'Arial, sans-serif',
+    maxWidth: '800px',
+    margin: 'auto',
+    padding: '20px',
+  },
+  heading: {
+    color: '#333',
+  },
+  table: {
+    width: '100%',
+    borderCollapse: 'collapse',
+    marginTop: '20px',
+  },
+  tableRow: {
+    padding: '10px',
+  },
+  formLabel: {
+    display: 'block',
+    marginBottom: '5px',
+    color: '#333',
+  },
+  formInput: {
+    padding: '8px',
+    width: '100%',
+    boxSizing: 'border-box',
+  },
+  addButton: {
+    backgroundColor: '#007BFF',
+    color: 'white',
+    padding: '10px',
+    border: 'none',
+    cursor: 'pointer',
+  },
+};
+
 const pageStyle = {
   textAlign: 'center',
   padding: '20px',
