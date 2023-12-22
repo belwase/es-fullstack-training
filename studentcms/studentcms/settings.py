2702b8c26af4bc1ab72d91dc32e2c297767a6651
ashishbw <techackernp@gmail.com>
2023-12-22 08:21:57 +0545

    Added swagger

--- studentcms/studentcms/settings.py
+++ studentcms/studentcms/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'drf_yasg',
 
     'userauth',
     'accounts',
--- studentcms/studentcms/urls.py
+++ studentcms/studentcms/urls.py
@@ -15,9 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path, path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from main.views import HomeView, TokenView
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="StudentCMS API",
+      default_version='v1',
+      description="StudentCMS API Documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="ab@gmail.com"),
+      license=openapi.License(name="GNU License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+   authentication_classes=(),
+)
 
 
 urlpatterns = [
@@ -34,5 +52,10 @@ urlpatterns = [
     # With RestFramework
     path('api/v1/student/', include('accounts.api_v1_urls')),
     path('api/v1/course/', include('courses.api_v1_urls')),
-    path('api/v1/', include('userauth.urls'))
+    path('api/v1/', include('userauth.urls')),
+
+    # Swagger docs
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
